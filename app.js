e6c36981bb0156541df29d1f56ddc34572723513
Malthe <eaa24mbb@students.eaaa.dk>
2025-04-23 11:39:35 +0200

    Register clientside

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import express, { urlencoded } from 'express';
 import session from 'express-session';
 import { routes } from './routes.js';
 import { render } from 'pug';
+import { ok } from 'assert';
 
 const app = express();
+
+let usersIds = 2;
 
 const users = [{
     username: 'Malthe',
@@ -64,7 +67,25 @@ app.get('/register', (request, response) => {
 })
 
 app.post('/register', (request, response) => {
-    
+    const { username, password, userlevel } = request.body;
+    request.session.isLoggedIn = true;
+
+    const user = {
+        username: username, 
+        password: password,
+        salt: "ccc",
+        dateCreated: Date(),
+        id: ++usersIds,
+        userlevel: userlevel
+    };
+
+    users.push(user);
+
+    response.send({
+        ok: true,
+        redirect: '/'
+    });
+
 })
 
 app.post('/login', (request, response) => {
--- assets/register.js
+++ assets/register.js
@@ -1,1 +1,45 @@
-console.log(object);
+const registerButton = document.getElementById("submitBtn");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const retypePasswordInput = document.getElementById('retypepass');
+const userlevelSelect = document.getElementById('userlevel');
+
+registerButton.addEventListener('click', async () => {
+    if (!checkInputs()) {
+        alert("All fields must be filled out!")
+    } else if (!checkPassword(passwordInput.value, retypePasswordInput.value)) {
+        alert("Passwords has to be the same!");
+        clearPasswords();
+    } else {
+        const response = await fetch('/register', {
+            method:"POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({
+                username: usernameInput.value,
+                password: passwordInput.value,
+                userlevel: userlevelSelect.value
+            })
+        })
+        const result = await response.json();
+        if(!result.ok) {
+            throw new Error(`Fejl: ${result.message}`);
+        } else {
+            window.location.href = result.redirect;
+        }
+    }
+})
+
+function checkPassword(password, retype) {
+    return password === retype;
+}
+
+function clearPasswords() {
+    passwordInput.clear;
+    retypePasswordInput.clear;
+}
+
+function checkInputs() {
+    return usernameInput.value || passwordInput.value || retypePasswordInput.value;
+}
