bc43458c978d4968b988c4c85dcf923b6155b2e3
andersna <andersna@stofanet.dk>
2025-05-07 12:00:45 +0200

    Delete fungerer ikke

---
+++ assets/usermessages.js
@@ -0,0 +1,20 @@
+const deleteBtns = document.getElementsByClassName('deleteBtns')
+console.log(deleteBtns);
+
+Array.from(deleteBtns).forEach(button =>{
+    button.addEventListener('click', async (event)=>{
+        event.preventDefault();
+        const messageId = button.getAttribute('data-id')
+        console.log(messageId);
+        let acceptDelete = confirm("ønsker du at slette beskeden?")
+        if(acceptDelete){
+            let response = await fetch(`/chats/messages/${messageId}`,{
+                method: "DELETE"    
+            })
+            const result = await response.json();
+            if(!result.ok){
+                throw new Error(`Fejl: ${result.message}`);
+            }
+        }
+    })
+})
